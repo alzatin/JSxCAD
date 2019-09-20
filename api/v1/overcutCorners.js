3007cdeac618e4c1c826813fc8ed21e40e51e5a5
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-09-20 10:31:50 +0300

    stash

--- api/v1/overcutCorners.js
+++ api/v1/overcutCorners.js
@@ -27,6 +27,21 @@ import { measureBoundingBox } from './measureBoundingBox';
  * :::
  **/
 
+const angle2 = (x1, y1, x2, y2) => {
+    // Use dotproduct to find angle between vectors
+    // This always returns an angle between 0, pi
+    const numer = (x1 * x2 + y1 * y2)
+    const denom = Math.sqrt((x1 ** 2 + y1 ** 2) * (x2 ** 2 + y2 ** 2))
+    return Math.acos(numer / denom) 
+}
+    
+const cross_sign = (x1, y1, x2, y2) => {
+    // True if cross is positive
+    // False if negative or zero
+    return x1 * y2 > x2 * y1
+}
+
+
 export const overcutCorners = (shape) => {
   
   var overcutPoints = [];
@@ -35,16 +50,20 @@ export const overcutCorners = (shape) => {
     var points = path
     points.push(path[0])
     points.push(path[1])
-    var i = 1;
-    while(i < points.length - 2){
-      const v1 = subtract(points[i-1], points[i])
-      const v2 = subtract(points[i], points[i+1])
-      const dotProduct = dot(v1, v2);
-      const crossProduct = cross(v1, v2)[3];
-      const angleComputed = angle(v1, v2);
-      console.log(crossProduct)
-      if(Math.abs(angleComputed - 1.570) < .01){ //We have a sharp angle
-        overcutPoints.push(points[i]);
+    var i = 2;
+    while(i < points.length){
+      const p1 = points[i]
+      const ref = points[i - 1]
+      const p2 = points[i - 2]
+      const x1 = p1[0] - ref[0]
+      const y1 = p1[1] - ref[1]
+      const x2 = p2[0] - ref[0]
+      const y2 = p2[1] - ref[1]
+
+      const angleComputed = angle2(x1, y1, x2, y2)
+      console.log(cross_sign(x1, y1, x2, y2))
+      if(cross_sign(x1, y1, x2, y2)){ //We have a sharp angle
+        overcutPoints.push(ref);
       }
       i++;
     }
